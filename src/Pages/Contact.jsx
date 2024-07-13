d1411aefe2eae9c43e4ebb2f2403e42ec11b8df6
Markel-Bradford <markel.bradford94@gmail.com>
2024-07-13 17:39:34 -0500

    Updated contact styles

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -23,15 +23,16 @@ const Contact = () => {
     };
   
     return (
-    <div>
+    <div className="contact">
     <h1 className="contact-title">Contact Us</h1>
     <div className="contact-container">
         <div className="contact-info">
         <h2>We'd love to hear from you!</h2>
-        <p>Your feedback us important to is. Whether you have enjoyed your experience
+        <p className="contact-msg">Your feedback us important to is. Whether you have enjoyed your experience
             or ran into difficulty on our platform, please feel free to contact us
             and we will be happy to assist you. Thank you for visiting Platinum Threads!
         </p>
+        <img className="contact-logo" src="/ProductTemplate/assets/pthreads_contact.png" alt="" />
     </div>
     <div className="form">
       <form ref={form} onSubmit={sendEmail} className="email-form">
@@ -54,7 +55,7 @@ const Contact = () => {
         </div>
         <div className="form-group">
           <label htmlFor="info">
-            Please provide details below:
+            Please provide feedback below:
           </label>
           <textarea name="info" id="info" cols="30" rows="10"></textarea>
         </div>
